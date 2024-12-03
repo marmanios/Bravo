9e42687e01b5fd4e8254c8aa3555325191ffb28c
Maged Armanios <maged.armanios@yahoo.com>
2024-12-03 14:29:09 -0500

    Cleanup

--- src/components/windows/transcript.tsx
+++ src/components/windows/transcript.tsx
@@ -68,50 +68,6 @@ export default function Transcript({ loading }: props) {
     }
   }, [transcript, lastUpdateLength, metadataMutation]);
 
-  // const [visibleCues, setVisibleCues] = useState<TTranscriptCue[]>([]);
-  // const visibleCuesRef = useRef(visibleCues); // Ref to track the real-time state
-  // const [isPlayingTranscript, setIsPlayingTranscript] = useState(false);
-
-  // Sync the ref with the current state
-  // useEffect(() => {
-  //   visibleCuesRef.current = visibleCues;
-  // }, [visibleCues]);
-
-  // useEffect(() => {
-  //   const playTranscript = async () => {
-  //     for (let i = 0; i < TEMPTRANSCRIPTCUES.length; i++) {
-  //       const newLine = TEMPTRANSCRIPTCUES[i];
-  //       const nextLine = TEMPTRANSCRIPTCUES[i + 1];
-  //       const timeToWait = (nextLine.start - newLine.start) * 1000;
-
-  //       // Add the new line only if it hasn't already been added
-  //       setVisibleCues((prevCues) => {
-  //         if (prevCues.length === i) {
-  //           return [...prevCues, newLine];
-  //         }
-  //         return prevCues; // No redundant update
-  //       });
-
-  //       if (i % 20 == 0 && i > 0) {
-  //         const texts: string[] = TEMPTRANSCRIPTCUES.slice(i - 20, i).map(
-  //           (cue) => cue.text
-  //         );
-  //         // metadataMutation.mutate({ text: texts.join("\n") });
-  //       }
-  //       await new Promise((resolve) => setTimeout(resolve, timeToWait));
-  // const element = document.getElementById("transcript-container");
-  // element!.scrollTop = element!.scrollHeight + 200;
-  //     }
-
-  //     return;
-  //   };
-
-  //   if (!isPlayingTranscript) {
-  //     setIsPlayingTranscript(true); // Prevent multiple loops
-  //     playTranscript();
-  //   }
-  // }, [isPlayingTranscript]);
-
   const formatMessageTime = (createdAt: string, timestamp: number) => {
     // Parse createdAt into a Date object
     const createdDate = new Date(createdAt);
@@ -153,12 +109,6 @@ export default function Transcript({ loading }: props) {
                 </div>
               </div>
             ))}
-          <div id={"cues-end"} className="flex">
-            {/* <p className="ml-auto uppercase">[Call Ended]</p>
-          <p className="ml-auto text-background">
-            {new Date().toLocaleTimeString()}
-          </p> */}
-          </div>
         </div>
       ) : (
         <div className="p-4 font-light grid place-content-center h-full">
